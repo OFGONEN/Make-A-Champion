48c01297d3c3c3dad5e51e227637c3a643ddc4d9
ofgonen <ofgonen@gmail.com>
2021-03-25 16:18:48 +0300

    Edit analytic settings

--- Assets/Scripts/FFStudio/FFAnalitic.cs
+++ Assets/Scripts/FFStudio/FFAnalitic.cs
@@ -1,62 +1,62 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// using Facebook.Unity;
-// using ElephantSDK;
+using Facebook.Unity;
+using ElephantSDK;
 
 namespace FFStudio
 {
     public class FFAnalitic : MonoBehaviour
     {
-        // private void Awake()
-        // {
-        //     if (!FB.IsInitialized)
-        //         FB.Init(OnFacebookInitialized, OnHideUnity);
-        //     else
-        //         FB.ActivateApp();
+        private void Awake()
+        {
+            if (!FB.IsInitialized)
+                FB.Init(OnFacebookInitialized, OnHideUnity);
+            else
+                FB.ActivateApp();
 
-        //     gameObject.AddComponent<ElephantCore>();
+            gameObject.AddComponent<ElephantCore>();
 
-        //     ElephantCore.onInitialized += OnElephantInitiliazed;
-        //     ElephantCore.onRemoteConfigLoaded += OnElephantRemoveConfigLoaded;
-        //     ElephantCore.onOpen += OnElephantOpen;
-        // }
+            ElephantCore.onInitialized += OnElephantInitiliazed;
+            ElephantCore.onRemoteConfigLoaded += OnElephantRemoveConfigLoaded;
+            ElephantCore.onOpen += OnElephantOpen;
+        }
 
-        // private void Start()
-        // {
-        //     Elephant.Init(false, true);
-        // }
+        private void Start()
+        {
+            Elephant.Init(false, true);
+        }
 
-        // void OnElephantInitiliazed()
-        // {
-        //     Debug.Log("[FFAnalitic] Elephant initiliazed");
-        // }
-        // void OnElephantRemoveConfigLoaded()
-        // {
-        //     Debug.Log("[FFAnalitic] Elephant Remote Config Loaded");
-        // }
-        // void OnElephantOpen(bool gdprRequired)
-        // {
+        void OnElephantInitiliazed()
+        {
+            Debug.Log("[FFAnalitic] Elephant initiliazed");
+        }
+        void OnElephantRemoveConfigLoaded()
+        {
+            Debug.Log("[FFAnalitic] Elephant Remote Config Loaded");
+        }
+        void OnElephantOpen(bool gdprRequired)
+        {
 
-        // }
-        // void OnFacebookInitialized()
-        // {
-        //     if (FB.IsInitialized)
-        //     {
-        //         FB.ActivateApp();
-        //         Debug.Log("[FFAnalitic] Facebook initiliazed");
-        //     }
-        //     else
-        //         Debug.Log("[FFAnalitic] Failed to initialize Facebook SDK");
+        }
+        void OnFacebookInitialized()
+        {
+            if (FB.IsInitialized)
+            {
+                FB.ActivateApp();
+                Debug.Log("[FFAnalitic] Facebook initiliazed");
+            }
+            else
+                Debug.Log("[FFAnalitic] Failed to initialize Facebook SDK");
 
 
-        //     DontDestroyOnLoad(gameObject);
+            DontDestroyOnLoad(gameObject);
 
-        // }
+        }
 
-        // void OnHideUnity(bool hide)
-        // {
+        void OnHideUnity(bool hide)
+        {
 
-        // }
+        }
     }
 }
